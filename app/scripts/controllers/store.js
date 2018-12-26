ba0716ec68e19358af28bb3ece0b57dcfdf5076a
Patrick <torralbapatrick15@gmail.com>
2018-12-26 12:17:04 -0600

    Add directives

--- app/scripts/controllers/store.js
+++ app/scripts/controllers/store.js
@@ -9,9 +9,48 @@
  */
 angular.module('sneakersApp')
   .controller('StoreCtrl', function () {
-    this.id = 'air-jordan-1-origin-story';
-    this.brand = 'Air Jordan 1';
-    this.name = 'Origin Story';
-    this.price = 160;
-    this.image = 'aj1-originstory.jpg';
+  	this.products = [
+  		{
+  			id: 'air-jordan-1-origin-story',
+    		brand: 'Air Jordan 1',
+    		name: 'Origin Story',
+    		desc: 'Some description...',
+    		price: 160,
+    		size: {
+    			9: 0,
+    			10: 100,
+    			11: 100,
+    			12: 0
+    		},
+    		image: 'aj1-originstory.jpg'
+    	},
+    	{
+  			id: 'air-fear-of-god-black',
+    		brand: 'Air Fear of God',
+    		name: 'Black',
+    		desc: 'Some description...',
+    		price: 350,
+    		size: {
+    			9: 0,
+    			10: 100,
+    			11: 100,
+    			12: 0
+    		},
+    		image: 'fog-black.jpg'
+    	},
+    	{
+  			id: 'air-jordan-11-concord',
+    		brand: 'Air Jordan XI',
+    		name: 'Concord',
+    		desc: 'Some description...',
+    		price: 220,
+    		size: {
+    			9: 0,
+    			10: 100,
+    			11: 100,
+    			12: 0
+    		},
+    		image: 'aj11-concord.jpg'
+    	}
+    ];
   });
