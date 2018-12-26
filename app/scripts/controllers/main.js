6a6a497b0012905eccb3305f641036021949d7d1
Patrick <torralbapatrick15@gmail.com>
2018-12-26 11:53:42 -0600

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sneakersApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sneakersApp
+ */
+angular.module('sneakersApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
