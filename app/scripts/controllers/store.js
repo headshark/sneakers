8b4aee4d302df63445abfc4258241f8837b92c73
Patrick <torralbapatrick15@gmail.com>
2018-12-26 12:39:17 -0600

    Add cart list and add to cart button

--- app/scripts/controllers/store.js
+++ app/scripts/controllers/store.js
@@ -10,4 +10,23 @@
 angular.module('sneakersApp')
   .controller('StoreCtrl', ['storeWarehouse', function(storeWarehouse) {
   	this.products = storeWarehouse.getProducts();
+  	this.cart = [];
+
+  	this.addToCart = function(product) {
+  		if (this.cart.length === 0) {
+			this.cart.push(product);
+  		} else {
+  			var repeat = false;
+  			for (var i=0; i<this.cart.length; i++) {
+  				if (this.cart[i].id === product.id) {
+  					repeat = true;
+  					product.limit = 'Sorry, you have reached the quantity limit. Please remove an item and try again.';
+  				}
+  			}
+
+  			if (!repeat) {
+  				this.cart.push(product);
+  			}
+  		}
+  	};
   }]);
