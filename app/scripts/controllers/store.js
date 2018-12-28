a04cb3da8dbda23fda0ba409e87293aadab38651
Patrick <torralbapatrick15@gmail.com>
2018-12-27 19:17:17 -0600

    Add styles to product

--- app/scripts/controllers/store.js
+++ app/scripts/controllers/store.js
@@ -11,20 +11,22 @@ angular.module('sneakersApp')
   .controller('StoreCtrl', ['storeWarehouse', function(storeWarehouse) {
   	this.products = storeWarehouse.getProducts();
   	this.cart = [];
+    this.selectedProduct;
 
   	/**
   	 * @description Add product on cart
   	 * @param {object} product
   	 */
-  	this.addToCart = function(product) {
+  	this.addToCart = function(product, index) {
   		if (this.cart.length === 0) {
 			this.cart.push(product);
   		} else {
   			var repeat = false;
   			for (var i=0; i<this.cart.length; i++) {
-  				if (this.cart[i].id === product.id) {
+  				if (this.cart[i].id === product.id) {        
   					repeat = true;
   					product.limit = 'Sorry, you have reached the quantity limit. Please remove an item and try again.';
+            this.selectedProduct = index;
   				}
   			}
 
@@ -43,5 +45,6 @@ angular.module('sneakersApp')
   		var index = this.cart.indexOf(product);
   		this.cart.splice(index, 1);
   		product.limit = '';
+      this.selectedProduct = -1;
   	};
   }]);
